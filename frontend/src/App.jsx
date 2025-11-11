d6bcbde3913bda0420b12493c97f94221f642b20
MaqboolAhmad01 <maqbool.riazahmad@gmail.com>
2025-11-11 22:33:20 +0500

    Add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 import React from 'react'
 import Quiz from './components/Quiz/quiz'
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,103 @@
+import logo from "../assets/logo.png"; // adjust path based on your folder
+import React, { useState } from "react";
+
+import "./login.css";
+
+
+const loginUser = async (formData) => {
+    try {
+      const response = await fetch("http://localhost:8000/auth/login/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+  
+      if (!response.ok) {
+        const err = await response.json();
+        throw new Error(err.message || "Login failed");
+      }
+  
+      return await response.json(); // return success data
+    } catch (error) {
+      throw error; // rethrow so caller can handle it
+    }
+  };
+
+const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await loginUser(form);
+      console.log("Login success:", data);
+      alert("Logged in successfully!");
+    } catch (err) {
+      console.error("Signup failed:", err.message);
+      alert("Error: " + err.message);
+    }
+  };
+
+  return (
+    <div>
+      {/* Logo on top-left */}
+      <div className="logo-container">
+        <img src={logo} alt="App Logo" className="logo" />
+      </div>
+
+      <div className="login-container">
+        {/* Right side card */}
+        <div className="login-card">
+          <h2 className="login-title">Login</h2>
+          <form onSubmit={handleSubmit}>
+
+            {/* Email */}
+            <div className="mb-3">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={form.email}
+                onChange={setForm}
+                className="form-control"
+                required
+              />
+            </div>
+
+            {/* Password */}
+            <div className="mb-3 position-relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                value={form.password}
+                className="form-control"
+                required
+              />
+              <i
+                className={`bi ${showPassword ? "bi-eye-slash" : "bi-eye"} toggle-password`}
+                onClick={() => setShowPassword(!showPassword)}
+              ></i>
+            </div>
+
+            <button type="submit" className="btn-login">
+              login
+
+            </button>
+            <div className="signup-link">
+              Don't have an account? <a href="/signup" >Sign up</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Login;
